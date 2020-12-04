203131560216d81b57f8c1573b4251e0976e0c7e
fapnip <fapnip@outlook.com>
2020-12-04 12:13:44 -0600

    Allow mboxdrive.com

--- src/mixins/Locator.js
+++ src/mixins/Locator.js
@@ -42,13 +42,14 @@ function avoidLast(value, array, randomGetter, locator, lookupPool, lasts) {
   return value
 }
 const supportedHosts = [
+  'https://www.mboxdrive.com/',
   'https://media*.vocaroo.com/', // https://vocaroo.com/
   'https://i.ibb.co', // imgbb.com
   'https://media.milovana.com',
   'https://iili.io', // Freeimage.host
 ]
 // TODO: break this out to multiple lines
-const allowedUrlMatcher = /(^(https:\/\/media[0-9]*\.vocaroo\.com\/.+|https:\/\/iili\.io\/.+|https:\/\/i\.ibb\.co\/.+|https:\/\/media\.milovana\.com\/.+|^data:image\/.+)|^(file:|gallery:).*\+\(\|oeos:(.+)\)$)/
+const allowedUrlMatcher = /(^(https:\/\/w*[0-9]*\.*mboxdrive\.com\/.+|https:\/\/media[0-9]*\.vocaroo\.com\/.+|https:\/\/iili\.io\/.+|https:\/\/i\.ibb\.co\/.+|https:\/\/media\.milovana\.com\/.+|^data:image\/.+)|^(file:|gallery:).*\+\(\|oeos:(.+)\)$)/
 
 export default {
   data: () => ({}),
