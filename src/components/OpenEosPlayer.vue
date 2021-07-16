4a66eb66dd5c1ddeedd1a5371835a7edc94bff9e
fapnip <fapnip@outlook.com>
2021-07-16 11:45:07 -0500

    Fix another type

--- src/components/OpenEosPlayer.vue
+++ src/components/OpenEosPlayer.vue
@@ -244,7 +244,7 @@
                 d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
               ></path>
             </svg>
-            in the upper right of the tease displayed on Molivana.)
+            in the upper right of the tease displayed on Milovana.)
           </p>
         </v-card-text>
       </v-card>
